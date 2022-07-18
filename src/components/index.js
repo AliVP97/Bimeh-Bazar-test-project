d36208a886d464b59cb1310878b25057aac806d2
Ali Valipour <85191682+AliVP97@users.noreply.github.com>
2022-07-19 01:17:33 +0430

    ✨ feat/(components/Alert)  Create Alert component

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Alert from "./Alert";
+import Items from "./Items";
+import SideFilter from "./SideFilter";
+import SortingFilter from "./SortingFilter";
+
+export { Alert, Items, SideFilter, SortingFilter };
