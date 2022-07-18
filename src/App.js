6a6f437c504f7555310690400304ddbea52bd950
Ali Valipour <85191682+AliVP97@users.noreply.github.com>
2022-07-19 01:19:09 +0430

    🐛 fix/(App.js)  Fix App.js & COMPLETE the app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,51 @@
-import OffersPage from "./OffersPage";
+import { useOffers } from "hooks";
 
-function App() {
-  return <OffersPage />;
-}
+import { OffersContext } from "contexts";
+import { SideFilter, Items, SortingFilter, Alert } from "components";
+
+import "./App.scss";
+
+const App = () => {
+  const {
+    data,
+    setData,
+    filteredData,
+    setFilteredData,
+    sortedData,
+    setSortedData,
+    sortFilter,
+    setSortFilter,
+    sideFilter,
+    setSideFilter,
+  } = useOffers();
+
+  return (
+    <OffersContext.Provider
+      value={{
+        data,
+        setData,
+        filteredData,
+        setFilteredData,
+        sortedData,
+        setSortedData,
+        sortFilter,
+        setSortFilter,
+        sideFilter,
+        setSideFilter,
+      }}
+    >
+      <div className="app">
+        <div className="side-filter">
+          <SideFilter />
+        </div>
+        <div className="main-items">
+          <Alert />
+          <SortingFilter />
+          <Items />
+        </div>
+      </div>
+    </OffersContext.Provider>
+  );
+};
 
 export default App;
